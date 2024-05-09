821f949cd828f8b9c4f0040d28c647c43d3ccaa8
neqq <supitcha.psr@gmail.com>
2024-05-09 10:39:38 +0700

    style: set up color palette in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#003045',
+        subPrimary: '#0D4F72',
+        darkPrimary: '#002434',
+        lightPrimary: '#EBF2FA',
+        secondary: '#48A393',
+        accent: '#E83550',
+        lightAccent: '#FFF1F1',
+        background: '#F6F6F6',
+        black: '#111111',
+        White: '#FEFEFE',
+        lightGrey: '#C8CDD0',
+        darkGrey: '#7E7E7E',
+        success: '#459E19',
+        warning: '#EDB95E',
+        error: '#CA2222',
+        information: '#4A90E2',
+      }
+    },
   },
   plugins: [],
 }
