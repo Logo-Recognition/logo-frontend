8bbdf0274a4f8e35de5bb3480e17601d8b4bd635
jerry2111 <jr_kaze@hotmail.co.th>
2024-05-27 11:39:08 +0700

    fix: add focusing icon color event

--- src/components/AugmentOptionBar.vue
+++ src/components/AugmentOptionBar.vue
@@ -1,39 +1,38 @@
 <script setup>
-import IconRotate from '@/components/icons/IconRotate.vue'
-import IconFlip from '@/components/icons/IconFlip.vue'
-import IconNoise from '@/components/icons/IconNoise.vue'
-import IconScailing from '@/components/icons/IconScailing.vue'
-import IconBright from '@/components/icons/IconBright.vue'
-import IconSaturation from '@/components/icons/IconSaturation.vue'
-import IconContrast from '@/components/icons/IconContrast.vue'
-import { ref } from 'vue'
+import IconRotate from '@/components/icons/IconRotate.vue';
+import IconFlip from '@/components/icons/IconFlip.vue';
+import IconNoise from '@/components/icons/IconNoise.vue';
+import IconScailing from '@/components/icons/IconScailing.vue';
+import IconBright from '@/components/icons/IconBright.vue';
+import IconSaturation from '@/components/icons/IconSaturation.vue';
+import IconContrast from '@/components/icons/IconContrast.vue';
+import { ref } from 'vue';
 import { defineEmits } from 'vue';
 
 const emit = defineEmits(['data-emitted']);
-const FocusingAugment = ref('Rotate')
+const FocusingAugment = ref('Rotate');
 
 const updateFocusingAugment = (augment) => {
-  FocusingAugment.value = augment
-  sendData()
-}
+  FocusingAugment.value = augment;
+  sendData();
+};
 
 const sendData = () => {
   const data = FocusingAugment.value; // Replace with actual data you want to pass
   emit('data-emitted', data);
 };
-
 </script>
 
 <template>
   <div id="OptionBar" class="flex justify-around">
     <div class="flex justify-around w-3/4 items-center">
-      <IconRotate class="cursor-pointer" id="icon" @click="updateFocusingAugment('Rotate')" />
-      <IconFlip class="cursor-pointer" @click="updateFocusingAugment('Flip')" />
-      <IconNoise class="cursor-pointer" @click="updateFocusingAugment('Noise')" />
-      <IconScailing class="cursor-pointer" @click="updateFocusingAugment('Scailing')" />
-      <IconBright class="cursor-pointer" @click="updateFocusingAugment('Brightness')" />
-      <IconSaturation class="cursor-pointer" @click="updateFocusingAugment('Saturation')" />
-      <IconContrast class="cursor-pointer" @click="updateFocusingAugment('Contrast')" />
+      <IconRotate class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Rotate' }" @click="updateFocusingAugment('Rotate')" />
+      <IconFlip class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Flip' }" @click="updateFocusingAugment('Flip')" />
+      <IconNoise class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Noise' }" @click="updateFocusingAugment('Noise')" />
+      <IconScailing class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Scailing' }" @click="updateFocusingAugment('Scailing')" />
+      <IconBright class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Brightness' }" @click="updateFocusingAugment('Brightness')" />
+      <IconSaturation class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Saturation' }" @click="updateFocusingAugment('Saturation')" />
+      <IconContrast class="cursor-pointer button" :class="{ 'active': FocusingAugment === 'Contrast' }" @click="updateFocusingAugment('Contrast')" />
     </div>
   </div>
 </template>
@@ -43,12 +42,18 @@ const sendData = () => {
   width: 100%;
   height: 60px;
   border-radius: 16px;
-  opacity: 0px;
   border: 1px solid #c8cdd0;
   display: flex;
-  align-items: center; /* Vertically center the content */
+  align-items: center;
+  opacity: 1;
 }
 #OptionBar > * {
   height: 80%; /* Ensures the icons do not exceed the height of the container */
+}
+.button {
+  color: #7E7E7E;
+}
+.button.active {
+  color: #7585FF;
 }
 </style>
