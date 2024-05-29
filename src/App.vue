c854ad98f7c7bd4c5bfbe22a161527876e6b6b68
neqq <supitcha.psr@gmail.com>
2024-05-29 16:04:03 +0700

    fix: adjust the CSS for full remaining space

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import { ref } from 'vue';
+import { ref } from 'vue'
 import SideBar from '@/components/SideBar.vue'
-import ModalUploadImage from './components/ModalUploadImage.vue';
+import ModalUploadImage from './components/ModalUploadImage.vue'
 const showModal = ref(false)
 
 const openModal = () => {
@@ -12,24 +12,30 @@ const openModal = () => {
 const closeModal = () => {
   showModal.value = false
 }
-
-
 </script>
 
 <template>
   <div class="app bg-grey-background">
-    <SideBar @open-modal="openModal"/>
-    <RouterView />
-    <ModalUploadImage 
-      :is-open="showModal"
-      @close="closeModal"
-    />
+    <SideBar @open-modal="openModal" />
+    <div class="content-container">
+      <RouterView />
+      <ModalUploadImage :is-open="showModal" @close="closeModal" />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .app {
-  display: flex;  
+  display: flex;
   flex-direction: row;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.content-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 }
 </style>
