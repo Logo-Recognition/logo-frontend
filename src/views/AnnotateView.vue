7586c6ca7cac2c4b01a5e7cea26c6efd59ba0d68
neqq <supitcha.psr@gmail.com>
2024-05-29 16:05:16 +0700

    feat: create an annotation component

--- src/views/AnnotateView.vue
+++ src/views/AnnotateView.vue
@@ -2,6 +2,9 @@
 import { ref, onMounted } from 'vue'
 import TabUnannotated from '@/components/TabUnannotated.vue'
 import TabAnnotated from '@/components/TabAnnotated.vue'
+import AnnotationCanvas from '@/components/AnnotationCanvas.vue'
+// eslint-disable-next-line no-unused-vars
+import LabelModal from '@/components/LabelModal.vue'
 import { API_URL } from '@/config.js'
 
 const currentTab = ref('Unannotated')
@@ -56,80 +59,115 @@ onMounted(() => {
 
 <template>
   <div class="annotate-container">
-    <h1 class="page-title text-dark">Annotate</h1>
-    <ul class="mb-5 text-sm flex list-none flex-row flex-wrap border-b-0 ps-0" role="tablist">
-      <li role="presentation">
-        <a
-          :class="getTabClass('Unannotated')"
-          @click="switchTab('Unannotated')"
-          role="tab"
-          aria-controls="tabs-unannotated"
-          aria-selected="currentTab === 'Unannotated'"
+    <h1 class="page-title text-lg text-dark font-bold">Annotate</h1>
+    <div class="annotation-section">
+      <AnnotationCanvas class="canvas-container" />
+      <div class="labels-container bg-white">
+        <h2>Labels</h2>
+        <div v-for="object in canvas?.getObjects()" :key="object.id" class="label-item">
+          Label for object
+        </div>
+      </div>
+      <div class="images-container">
+        <ul class="mb-5 text-sm flex list-none flex-row flex-wrap border-b-0 ps-0" role="tablist">
+          <li role="presentation">
+            <a
+              :class="getTabClass('Unannotated')"
+              @click="switchTab('Unannotated')"
+              role="tab"
+              aria-controls="tabs-unannotated"
+              aria-selected="currentTab === 'Unannotated'"
+            >
+              Unannotated
+              <span
+                class="badge"
+                :class="{
+                  'bg-primary text-light': currentTab === 'Unannotated',
+                  'outline-badge border-grey-text': currentTab !== 'Unannotated'
+                }"
+                >{{ unannotatedImages.length }}</span
+              >
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              :class="getTabClass('Annotated')"
+              @click="switchTab('Annotated')"
+              role="tab"
+              aria-controls="tabs-annotated"
+              aria-selected="currentTab === 'Annotated'"
+            >
+              Annotated
+              <span
+                class="badge"
+                :class="{
+                  'bg-primary text-light': currentTab === 'Annotated',
+                  'outline-badge border-grey-text': currentTab !== 'Annotated'
+                }"
+                >{{ annotatedImages.length }}</span
+              >
+            </a>
+          </li>
+        </ul>
+        <div
+          v-if="currentTab === 'Unannotated'"
+          class="tab-content"
+          id="tabs-unannotated"
+          role="tabpanel"
         >
-          Unannotated
-          <span
-            class="badge"
-            :class="{
-              'bg-primary text-light': currentTab === 'Unannotated',
-              'outline-badge border-grey-text': currentTab !== 'Unannotated'
-            }"
-            >{{ unannotatedImages.length }}</span
-          >
-        </a>
-      </li>
-      <li role="presentation">
-        <a
-          :class="getTabClass('Annotated')"
-          @click="switchTab('Annotated')"
-          role="tab"
-          aria-controls="tabs-annotated"
-          aria-selected="currentTab === 'Annotated'"
+          <TabUnannotated :unannotatedImages="unannotatedImages" />
+        </div>
+        <div
+          v-else-if="currentTab === 'Annotated'"
+          class="tab-content"
+          id="tabs-annotated"
+          role="tabpanel"
         >
-          Annotated
-          <span
-            class="badge"
-            :class="{
-              'bg-primary text-light': currentTab === 'Annotated',
-              'outline-badge border-grey-text': currentTab !== 'Annotated'
-            }"
-            >{{ annotatedImages.length }}</span
-          >
-        </a>
-      </li>
-    </ul>
-    <div
-      v-if="currentTab === 'Unannotated'"
-      class="tab-content"
-      id="tabs-unannotated"
-      role="tabpanel"
-    >
-      <TabUnannotated :unannotatedImages="unannotatedImages" />
-    </div>
-    <div
-      v-else-if="currentTab === 'Annotated'"
-      class="tab-content"
-      id="tabs-annotated"
-      role="tabpanel"
-    >
-      <TabAnnotated :annotatedImages="annotatedImages" />
+          <TabAnnotated :annotatedImages="annotatedImages" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .annotate-container {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  padding-top: 32px;
-  padding-left: 40px;
-  padding-right: 60px;
+  padding: 32px 60px 40px 40px;
+}
+
+.annotation-section {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 32px 24px;
 }
 
 .page-title {
   margin-bottom: 16px;
-  font-size: 18px;
-  font-weight: bold;
+}
+
+.canvas-container {
+  grid-column: 1 / 2;
+  grid-row: 1 / 3;
+}
+
+.labels-container {
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  border-radius: 16px;
+  padding: 16px;
+  overflow-y: auto;
+}
+
+.label-item {
+  padding: 4px;
+  border-bottom: 1px solid #d8dbd8;
+}
+
+.images-container {
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  overflow-y: auto;
 }
 
 .tab-content {
