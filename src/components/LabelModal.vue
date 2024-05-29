a6aa487326c9db592a1f3471c71575505ff68cd1
neqq <supitcha.psr@gmail.com>
2024-05-30 02:08:55 +0700

    feat: get class list

--- src/components/LabelModal.vue
+++ src/components/LabelModal.vue
@@ -1,26 +1,61 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { API_URL } from '@/config.js'
+import axios from 'axios'
 
 const emit = defineEmits(['close', 'submit'])
 const boxName = ref('')
+const classList = ref([])
+const selectedClass = ref(null)
+
+const fetchClassList = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/api/class`, { method: 'GET' })
+    classList.value = response.data
+  } catch (error) {
+    console.error('Error fetching class list:', error)
+  }
+}
 
 const submitBoxName = () => {
-  emit('submit', boxName.value)
-  boxName.value = ''
+  if (selectedClass.value) {
+    emit('submit', { boxName: boxName.value, className: selectedClass.value })
+    boxName.value = ''
+    selectedClass.value = null
+  } else {
+    alert('Please select a class for the box.')
+  }
 }
 
 const cancelBoxDrawing = () => {
   boxName.value = ''
+  selectedClass.value = null
   emit('close', true)
 }
+
+const selectClass = (className) => {
+  selectedClass.value = className
+}
+
+onMounted(() => {
+  fetchClassList()
+})
 </script>
 
 <template>
   <div class="modal" @click.self="cancelBoxDrawing">
     <div class="modal-content" @click.stop>
-      <div class="box">
-        <label class="label">Box Name:</label>
-        <input class="input" type="text" v-model="boxName" autofocus />
+      <div class="class-list">
+        <label class="label">Select Class:</label>
+        <div
+          class="class-item"
+          v-for="classItem in classList"
+          :key="classItem.id"
+          @click="selectClass(classItem.name)"
+          :class="{ selected: selectedClass.value === classItem.name }"
+        >
+          {{ classItem.name }}
+        </div>
       </div>
       <div class="buttons">
         <button class="button is-success" @click="submitBoxName">Submit</button>
@@ -68,6 +103,23 @@ const cancelBoxDrawing = () => {
   border-color: black;
 }
 
+.class-list {
+  margin-bottom: 20px;
+}
+
+.class-item {
+  padding: 8px;
+  border: 1px solid grey;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 4px;
+}
+
+.class-item.selected {
+  background-color: #42b983;
+  color: white;
+}
+
 .buttons {
   display: flex;
   justify-content: center;
@@ -78,6 +130,7 @@ const cancelBoxDrawing = () => {
   color: white;
   border-radius: 4px;
   padding: 5px;
+  margin-right: 5px;
 }
 
 .button.is-danger {
