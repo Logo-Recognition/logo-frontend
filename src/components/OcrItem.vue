c8e4bfc884458bf65a00ca7b77570e7a15820399
aotsurasak46 <aotsurasak46@gmail.com>
2024-06-11 14:20:10 +0700

    feat : add OCR view

---
+++ src/components/OcrItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  imageSrc: String,
+  text: String,
+})
+</script>
+
+<template>
+    <div id="row-container">
+        <div id="img-section">
+          <img :src="props.imageSrc" crossorigin="anonymous">
+        </div>
+        <div id="txt-section">
+          <p>{{ props.text }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#row-container{
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+}
+
+</style>
---
+++ src/views/OcrView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import OcrItem from '@/components/OcrItem.vue';
+import UploadImageOcr from '@/components/UploadImageOcr.vue'
+import { ref } from 'vue';
+const processed_data = ref([])
+const onUpdateProcessedData = (data) => {
+  processed_data.value = data
+}
+
+
+</script>
+
+<template>
+  <main class="flex-col py-10 px-10">
+    <span id="title" class="text-lg font-medium">OCR</span>
+    <UploadImageOcr @send-data="onUpdateProcessedData"/>
+    <div v-for="(item, index) in processed_data" :key="index">
+      <OcrItem :image-src="item['image_url']" :text="item['text']"></OcrItem>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
