75d11d62e9e199352674222fbf6a2f7a9f86378e
jerry2111 <jr_kaze@hotmail.co.th>
2024-06-13 09:49:40 +0700

    chore:install vue lazyload

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'vue3-toastify/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persist'
+import VueLazyload from 'vue-lazyload'
 import './assets/main.css'
 
 import App from './App.vue'
@@ -17,5 +18,11 @@ app.use(router)
 app.use(Vue3Toastify, {
   position: 'top-right',
   transition: 'Vue-Toastify__bounce'
+})
+app.use(VueLazyload, {
+  preLoad: 1.3,
+  // error: 'https://lottie.host/embed/01b7b699-1dbe-422c-adce-24c9afe61bdd/1ti45YAMqN.json',
+  // loading: '@/assets/LoadinfGif.vue',
+  attempt: 1
 })
 app.mount('#app')
