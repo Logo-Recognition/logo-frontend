385129ae65931a5f9b78463961e9b1aa8c07b1a8
neqq <supitcha.psr@gmail.com>
2024-10-21 17:36:24 +0700

    feat: implement login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<script setup>
+// Add any JavaScript logic or component imports here
+import { ref } from 'vue';
+
+const message = ref('Hello, Vue 3!');
+</script>
+
+<template>
+  <div class="new-page">
+    <h1>New Page</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+.new-page {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  font-size: 2rem;
+  color: #42b983;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
