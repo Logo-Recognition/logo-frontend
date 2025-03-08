5ab2fd86d7bad72177e08f94c9e37666792352f9
neqq <supitcha.psr@gmail.com>
2025-03-08 15:13:21 +0700

    feat: enhance ScrapeData component with date validation and emit scraping completion event

--- src/components/ScrapeData.vue
+++ src/components/ScrapeData.vue
@@ -1,18 +1,19 @@
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue'
+import { ref, computed, onMounted, onUnmounted, defineEmits, watch } from 'vue'
 import { API_URL } from '@/config.js'
 import { toast } from 'vue3-toastify'
-import JSZip from 'jszip'
 
 const keyword = ref('')
 const startDate = ref('')
 const endDate = ref('')
 const searchQuery = ref('')
-const isLoading = ref(false)
 const isDropdownOpen = ref(false)
-const currentImageIndex = ref(0)
+const dropdownRef = ref(null)
+const isLoading = ref(false)
 const processedImageUrls = ref([])
 const tweetUrls = ref([])
+
+const emits = defineEmits(['scraping-completed'])
 
 const brands = [
   'Adidas',
@@ -59,24 +60,48 @@ const selectKeyword = (brand) => {
   isDropdownOpen.value = false
 }
 
-const dropdownRef = ref(null)
 const closeDropdown = (event) => {
   if (dropdownRef.value && !dropdownRef.value.contains(event.target)) {
     isDropdownOpen.value = false
   }
 }
 
-onMounted(() => {
-  document.addEventListener('click', closeDropdown)
+onMounted(() => document.addEventListener('click', closeDropdown))
+onUnmounted(() => document.removeEventListener('click', closeDropdown))
+
+const minStartSelectableDate = computed(() => {
+  const today = new Date()
+  today.setDate(today.getDate() - 7)
+  return today.toISOString().split('T')[0]
 })
 
-onUnmounted(() => {
-  document.removeEventListener('click', closeDropdown)
+const minEndSelectableDate = computed(() => {
+  if (startDate.value) {
+    return startDate.value
+  }
+
+  const today = new Date()
+  today.setDate(today.getDate() - 7)
+  return today.toISOString().split('T')[0]
+})
+
+const maxSelectableDate = computed(() => {
+  const today = new Date()
+  today.setDate(today.getDate() - 1)
+  return today.toISOString().split('T')[0] // Format as YYYY-MM-DD
+})
+
+watch(startDate, (newStartDate) => {
+  if (endDate.value && endDate.value < newStartDate) {
+    endDate.value = ''
+  }
 })
 
 const startScraping = async () => {
   if (!keyword.value || !startDate.value || !endDate.value) {
-    alert('Please fill in all fields before scraping.')
+    toast.warn('Please fill in all fields before scraping.', {
+      autoClose: 3000
+    })
     return
   }
 
@@ -84,10 +109,7 @@ const startScraping = async () => {
 
   try {
     const response = await fetch(
-      `${API_URL}/api/scrape?keyword=${encodeURIComponent(keyword.value)}&start_date=${startDate.value}&end_date=${endDate.value}`,
-      {
-        method: 'GET'
-      }
+      `${API_URL}/api/scrape?keyword=${encodeURIComponent(keyword.value)}&start_date=${startDate.value}&end_date=${endDate.value}`
     )
 
     if (!response.ok) {
@@ -98,83 +120,26 @@ const startScraping = async () => {
     console.log('Scraping response:', data)
 
     if (data.predicted_url && Array.isArray(data.predicted_url)) {
-      processedImageUrls.value = data.predicted_url.map((item) => item.url).filter(Boolean)
+      processedImageUrls.value = data.predicted_url.map((item) => item.image_url).filter(Boolean)
       tweetUrls.value = data.predicted_url.map((item) => item.tweet_url).filter(Boolean)
+      emits('scraping-completed', processedImageUrls.value)
     } else {
       console.error('Unexpected response format', data)
-      alert('Error: No images found.')
+      toast.error('Error: No images found.', {
+        autoClose: 3000
+      })
     }
-
-    alert('Scraping started successfully!')
+    toast.success('Scraping started successfully!', {
+      autoClose: 3000
+    })
   } catch (error) {
     console.error(error)
-    alert('An error occurred while starting the scraping.')
+    toast.error('An error occurred while starting the scraping.', {
+      autoClose: 3000
+    })
   } finally {
     isLoading.value = false
   }
-}
-
-const downloadAllImages = async () => {
-  if (processedImageUrls.value.length === 0) {
-    toast.error('No images to download.', {
-      autoClose: 3000
-    })
-    return
-  }
-
-  try {
-    const zip = new JSZip()
-    const folder = zip.folder('images')
-
-    for (let i = 0; i < processedImageUrls.value.length; i++) {
-      const imageUrl = processedImageUrls.value[i]
-      const filename = `image_${i + 1}.jpg`
-
-      const response = await fetch(imageUrl)
-      const imageBlob = await response.blob()
-      folder.file(filename, imageBlob)
-    }
-
-    const zipBlob = await zip.generateAsync({ type: 'blob' })
-    const url = window.URL.createObjectURL(zipBlob)
-    const link = document.createElement('a')
-    link.href = url
-    link.download = 'images.zip'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    window.URL.revokeObjectURL(url)
-
-    toast.success('Images downloaded successfully!', {
-      autoClose: 3000
-    })
-  } catch (error) {
-    toast.error('Error downloading images.', {
-      autoClose: 3000
-    })
-    console.error('Error downloading images:', error)
-  }
-}
-
-const showNextImage = () => {
-  if (currentImageIndex.value < processedImageUrls.value.length - 1) {
-    currentImageIndex.value++
-  } else {
-    currentImageIndex.value = 0
-  }
-}
-
-const showPreviousImage = () => {
-  if (currentImageIndex.value > 0) {
-    currentImageIndex.value--
-  } else {
-    currentImageIndex.value = processedImageUrls.value.length - 1
-  }
-}
-
-// Function to set the current image index
-const setImageIndex = (index) => {
-  currentImageIndex.value = index
 }
 </script>
 
@@ -206,12 +171,26 @@ const setImageIndex = (index) => {
 
     <div>
       <label class="block text-gray-700 font-semibold">Start Date</label>
-      <input type="date" v-model="startDate" class="w-full mt-1 p-2 border rounded-lg" />
+      <input
+        type="date"
+        v-model="startDate"
+        :min="minStartSelectableDate"
+        :max="maxSelectableDate"
+        :class="{ 'has-value': startDate !== '' }"
+        class="w-full mt-1 p-2 border rounded-lg"
+      />
     </div>
 
     <div>
       <label class="block text-gray-700 font-semibold">End Date</label>
-      <input type="date" v-model="endDate" class="w-full mt-1 p-2 border rounded-lg" />
+      <input
+        type="date"
+        v-model="endDate"
+        :min="minEndSelectableDate"
+        :max="maxSelectableDate"
+        :class="{ 'has-value': endDate !== '' }"
+        class="w-full mt-1 p-2 border rounded-lg"
+      />
     </div>
   </div>
 
@@ -224,84 +203,6 @@ const setImageIndex = (index) => {
     >
       {{ isLoading ? 'Scraping...' : 'Start Scraping' }}
     </button>
-  </div>
-
-  <!-- Display while images are being processed -->
-  <div v-if="isLoading">
-    <div id="show-picture-card">
-      <div class="flex justify-between">
-        <div class="flex flex-col">
-          <h3>Result</h3>
-        </div>
-        <div class="navigation-buttons flex items-center">
-          <button
-            @click="toggleDropdown"
-            :class="{ active: showDropdown }"
-            id="predicted-bar-button"
-            disabled
-          >
-            <IconView />
-          </button>
-          <button @click="downloadAllImages" disabled><IconDownload /></button>
-        </div>
-      </div>
-      <div class="show-predicted flex justify-around">
-        <div id="the-predicted-image" class="grid place-content-center"><LoadingIndicator /></div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Display after images are processed -->
-  <div v-if="processedImageUrls.length > 0">
-    <div id="show-picture-card" v-if="processedImageUrls.length > 0">
-      <div class="flex justify-between items-center">
-        <div class="flex items-center flex-1">
-          <div class="flex flex-col">
-            <h3>Result</h3>
-          </div>
-        </div>
-
-        <div class="flex-1 text-center">
-          <p>{{ currentImageIndex + 1 }} / {{ processedImageUrls.length }}</p>
-        </div>
-
-        <div class="navigation-buttons flex items-center flex-1 justify-end">
-          <button
-            @click="toggleDropdown"
-            :class="{ active: showDropdown }"
-            id="predicted-bar-button"
-          >
-            <IconView />
-          </button>
-          <button @click="downloadAllImages"><IconDownload /></button>
-        </div>
-      </div>
-      <div class="show-predicted flex justify-around">
-        <button @click="showPreviousImage"><IconArrowL /></button>
-        <img
-          v-if="processedImageUrls.length > 0"
-          :src="processedImageUrls[currentImageIndex]"
-          alt="Processed Image"
-          crossorigin="anonymous"
-          id="the-predicted-image"
-        />
-        <button @click="showNextImage"><IconArrowR /></button>
-      </div>
-      <div v-if="showDropdown" class="mt-2">
-        <div class="preview-container">
-          <div class="preview-area">
-            <div v-for="(image, index) in processedImageUrls" :key="index" class="preview-items">
-              <img
-                v-lazy="image"
-                class="preview-img"
-                @click="setImageIndex(index)"
-                crossorigin="anonymous"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -317,96 +218,17 @@ const setImageIndex = (index) => {
   padding: auto;
 }
 
-#show-picture-card {
-  width: 95%; /* Adjust the width percentage as needed */
-  height: auto;
-  border-radius: 16px;
-  background-color: #fefefe;
-  padding: 20px;
-  margin: 30px;
-  background-color: #ffffff;
+input[type='date']::-webkit-datetime-edit-text,
+input[type='date']::-webkit-datetime-edit-month-field,
+input[type='date']::-webkit-datetime-edit-day-field,
+input[type='date']::-webkit-datetime-edit-year-field {
+  color: #9ca3af;
 }
 
-#show-picture-card h3 {
-  font-size: 20px;
-  font-weight: 700;
-}
-
-#the-predicted-image {
-  width: 70%; /* Limit the maximum width */
-  height: 400px; /* Maintain aspect ratio */
-  object-fit: contain; /* Ensure the entire image fits within the set dimensions */
-  /* border-radius: 16px; */
-  border: 1px dashed #3745be;
-  padding: auto;
-}
-
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr); /* 3 columns */
-  gap: 10px;
-  justify-items: center;
-}
-
-.grid-item {
-  width: 100%;
-  height: auto;
-  padding: 5px;
-}
-
-.grid-item img {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-}
-
-.dropdown-container {
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 300px; /* Adjust as needed */
-  background-color: white;
-  border: 1px solid #ccc;
-  padding: 10px;
-  position: absolute;
-  z-index: 1000;
-}
-
-.dropdown-item {
-  width: 50px;
-  height: 50px;
-  margin: 5px;
-  cursor: pointer;
-}
-
-.dropdown-item img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.preview-area {
-  display: inline-flex; /* Use flexbox layout */
-  gap: 10px; /* Add spacing between items */
-}
-
-.preview-items {
-  flex: 0 0 auto; /* Prevent items from shrinking */
-}
-
-.preview-img {
-  border-radius: 10px;
-  width: 150px;
-  height: 100px;
-  object-fit: fill;
-  cursor: pointer;
-  margin-top: 5px;
-}
-
-#predicted-bar-button {
-  color: #5a5d6c;
-}
-
-#predicted-bar-button.active {
-  color: #7585ff;
+input[type='date'].has-value::-webkit-datetime-edit-text,
+input[type='date'].has-value::-webkit-datetime-edit-month-field,
+input[type='date'].has-value::-webkit-datetime-edit-day-field,
+input[type='date'].has-value::-webkit-datetime-edit-year-field {
+  color: #000000;
 }
 </style>
